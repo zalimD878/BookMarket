d4c425e001a2a38433adf495c9195b5e069cda91
zalimD878 <z.dabagov@mail.ru>
2025-08-01 22:52:53 +0300

    fix list

--- src/components/BooksList.tsx
+++ src/components/BooksList.tsx
@@ -1,5 +1,6 @@
 import type { Book } from "../types/books";
 import "../App.css";
+import bookImage from "../assets/bookImage.svg";
 
 interface Props {
   bookList: Book[];
@@ -13,13 +14,18 @@ export function Books({ bookList }: Props) {
           <div className="book-card">
             <img
               className="img-title"
-              src={book.volumeInfo.imageLinks.thumbnail}
+              src={
+                book.volumeInfo.imageLinks
+                  ? book.volumeInfo.imageLinks.thumbnail
+                  : bookImage
+              }
               alt="book image"
             />
             <h4>{book.volumeInfo.title}</h4>
             <h4>{book.volumeInfo.authors}</h4>
             <h4>{book.volumeInfo.publishedDate}</h4>
             <h4>{book.volumeInfo.publisher}</h4>
+            {/* <button onClick={handleBookCard}>more...</button> */}
           </div>
         );
       })}
